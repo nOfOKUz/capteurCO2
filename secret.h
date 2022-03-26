5c8ed251e3fc0241804368421a31ddff455fb1f1
nOfOKUz <102419404+nOfOKUz@users.noreply.github.com>
2022-03-26 14:53:33 +0100

    Add files via upload

---
+++ secret.h
@@ -0,0 +1,24 @@
+#define EMAIL_PRO 	  	  "you@your_provider.something"
+#define EMAIL_ANONI 		  "anonymous@your_provider.something"
+#define ME_USER			  "your_login"
+#define ME_PWD			  "your_password"
+#define SSID_PRO		  "your_wpa2_ent_ssid"
+#define NTP_PRO		 	  "ntp.your_provider.something"
+#define PROXY1_PRO		  "your_firt_proxy_adresse"
+#define PROXY2_PRO	  	  "your_second_proxy_adresse"
+#define PROXY_PORT_PRO 		   8080 // or other port number
+
+#define SSID_RES1	  	  "ssid1"
+#define PWD_RES1	  	  "password1"
+
+#define SSID_RES2		  "ssid2"
+#define PWD_RES2		  "password2"
+
+#define SSID_RES3		  "ssid3"
+#define PWD_RES3		  "password3"
+
+#define SSID_RES4		  "ssid4"
+#define PWD_RES4	    	  "password4"
+
+#define ME_APIKEY	    	  "your_write_api_key_at_thngspeak"
+#define ME_CHANEL	  	  your_channel_number
